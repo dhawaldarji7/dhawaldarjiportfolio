5b5ab6bee2fc43a5bc1251b195de734e5c514a2c
Dhawal Darji <dhawal.d7@gmail.com>
2021-06-24 20:25:04 -0400

    My portfolio developed using React

---
+++ src/Components/About.js
@@ -0,0 +1,40 @@
+import React from "react";
+
+const About = ({ data }) => {
+  if (data) {
+    var profilepic = "images/" + data.image;
+    var bio = data.bio;
+    var resumeDownload = data.resumedownload;
+  }
+
+  return (
+    <section id="about">
+      <div className="row">
+        <div className="three columns">
+          <img
+            className="profile-pic"
+            src={profilepic}
+            alt="Dhawal's Profile Pic"
+          />
+        </div>
+        <div className="nine columns main-col">
+          <h2>
+            <span>About Me</span>
+          </h2>
+          <p>{bio}</p>
+        </div>
+        <div className="row">
+          <div className="columns download">
+            <p>
+              <a href={resumeDownload} className="button">
+                <i className="fa fa-download"></i>Resume
+              </a>
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default About;
---
+++ src/Components/Footer.js
@@ -0,0 +1,42 @@
+import React from "react";
+
+const Footer = ({ data }) => {
+  if (data) {
+    var networks = data.social.map(function (network) {
+      return (
+        <li key={network.name}>
+          <a href={network.url}>
+            <i className={network.className}></i>
+          </a>
+        </li>
+      );
+    });
+  }
+
+  return (
+    <footer>
+      <div className="row">
+        <div className="twelve columns">
+          <ul className="social-links">{networks}</ul>
+
+          <ul>
+            <li style={{ color: "grey" }}>
+              Made while consuming chai by Dhawal
+            </li>
+
+            <ul className="copyright">
+              <li>©️ All rights reserved</li>
+            </ul>
+          </ul>
+        </div>
+        <div id="go-top">
+          <a className="smoothscroll" title="Back to Top" href="#home">
+            <i className="icon-up-open"></i>
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
